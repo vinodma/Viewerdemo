dfc9cdb2df6e56d74d2d12a57d2a18bf1e264dd1
vinodma <vinod.mangipudi@soteradefense.com>
2016-08-10 11:06:05 -0400

    Update protein_label_dictionary.R

--- external/protein_label_dictionary.R
+++ external/protein_label_dictionary.R
@@ -6,7 +6,7 @@ appendlabel<-function(x){
     
     lbl<-strsplit(mp[[x]],split=" ")
     lapply(lbl,function(y){new<-data.frame(Pathway=y,Protein=x);protienDSpathway<<-rbind(protienDSpathway,new)})
-  
+    
     
   }
   
@@ -62,7 +62,7 @@ getrawentititesfromComm<-function(id,con=NULL){
         lapply(jsobj$nodes$name,appendlabel )
         
       }
-
+      
     }
     
   }
@@ -99,31 +99,31 @@ getpie<- function(x){
 }
 
 getproteinlabeldict <- function(){ 
-con <- file("./www/data/GESA_Canonical_pathways_c2.cp.v5.0.symbols.gmt_filtered.tsv") 
-open(con);
-results.list <- list();
-current.line <- 1
-map <- new.env(hash=T, parent=emptyenv())
-while (length(line <- readLines(con, n = 1, warn = FALSE)) > 0) {
-  
-  dt <- strsplit(line, split="\t")
-  val<-unlist(dt[[1]][1])
-  keys<-unlist(dt[[1]][-1])
-  for(key in keys){
-    if(key %in% names(map)){
-     oldval <- map[[key]]
-     map[[key]] <- paste(oldval,val,sep=" ") 
-    }
-    else
-    {
-    map[[key]] <- val
-    }
+  con <- file("./www/data/GESA_Canonical_pathways_c2.cp.v5.0.symbols.gmt_filtered.tsv") 
+  open(con);
+  results.list <- list();
+  current.line <- 1
+  map <- new.env(hash=T, parent=emptyenv())
+  while (length(line <- readLines(con, n = 1, warn = FALSE)) > 0) {
+    
+    dt <- strsplit(line, split="\t")
+    val<-unlist(dt[[1]][1])
+    keys<-unlist(dt[[1]][-1])
+    for(key in keys){
+      if(key %in% names(map)){
+        oldval <- map[[key]]
+        map[[key]] <- paste(oldval,val,sep=" ") 
+      }
+      else
+      {
+        map[[key]] <- val
+      }
     }
     
-  #results.list[[current.line]] <- toString(unlist(strsplit(line, split="\t")))
-  current.line <- current.line + 1
-} 
-close(con)
-
-return(map)
+    #results.list[[current.line]] <- toString(unlist(strsplit(line, split="\t")))
+    current.line <- current.line + 1
+  } 
+  close(con)
+  
+  return(map)
 }
